f91814df185df318eaf304eb32c2f59f96340527
Nam Luu <nam.luu@isobar.com>
2022-06-23 23:06:35 +0700

    reset source code

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('welcome');
 });
-
-Route::get('/info', function () {
-    phpinfo();
-});
-
-require __DIR__.'/auth.php';
