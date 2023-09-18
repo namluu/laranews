969d281cce63d742258f915ccb7a0836f6331ac9
Nam Luu <nam.luu@merkle.com>
2023-09-18 23:02:28 +0700

    get back project & add admin seeder

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -1,15 +1,15 @@
 <?php
- 
+
 namespace App\Http\Controllers\Admin;
- 
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
- 
+
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard');
+        return view('admin/dashboard');
     }
 }
